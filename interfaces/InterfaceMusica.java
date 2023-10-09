16a84055471639f19fb65485d2e4dd6a6f7087ab
Jordan Lima <60404264+Jordan-Lima@users.noreply.github.com>
2023-10-09 16:17:15 -0300

    Create InterfaceMusica.java

---
+++ interfaces/InterfaceMusica.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface InterfaceMusica {
+    void play();
+    void stop();
+    void back();
+    void next();
+    void volumePlus();
+    void volumeMinus();
+}
